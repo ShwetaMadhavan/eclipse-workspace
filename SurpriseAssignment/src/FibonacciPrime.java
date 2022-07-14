44adcf545d203b63d53099cbe1d95430412b45f3
ShwetaMadhavan <46585864+ShwetaMadhavan@users.noreply.github.com>
2022-07-14 13:47:53 +0530

    new

--- SurpriseAssignment/src/FibonacciPrime.java
+++ SurpriseAssignment/src/FibonacciPrime.java
@@ -5,9 +5,9 @@ public class FibonacciPrime {
 		// TODO Auto-generated method stub
 		int a=0,b=1,c=0;
 		int n=0;
-		System.out.println(a);
-		System.out.println(b);
-		while(n<8) {
+		//System.out.println(a);
+		//System.out.println(b);
+		while(n<10) {
 			int count=0;
 			c=a+b;
 			a=b;
